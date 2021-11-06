5c0132db014b02416dd35948df0f0da0dbb2ed5b
Syed Ahsan <syedahsan9682@gmail.com>
2021-11-06 21:15:23 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require("express");
 const hbs = require('hbs');
 const app = express();
-const port = pocess.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 // Builtin Midileware
 const staticPath = path.join(__dirname, "../public");
